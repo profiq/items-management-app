cc1f26d439ac0f5fe1024e617e6efd29040ac5f0
Jan Stránský <jan.stransky@profiq.com>
2026-01-14 14:09:49 +0000

    Resolve "Integrate swagger to the NestJS app"

--- backend/src/hello/hello.controller.ts
+++ backend/src/hello/hello.controller.ts
@@ -1,16 +1,37 @@
 import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
 import { HelloService } from './hello.service';
+import {
+  ApiBadRequestResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+} from '@nestjs/swagger';
 
 @Controller('hello')
 export class HelloController {
   constructor(private helloService: HelloService) {}
 
   @Get()
+  @ApiOkResponse({ example: { hello: 'world' } })
   async getHello(): Promise<{ hello: string }> {
     return this.helloService.getHello();
   }
 
   @Get(':id')
+  @ApiOkResponse({ example: { hello_id: 1 } })
+  @ApiNotFoundResponse({
+    example: {
+      message: 'Id not found',
+      error: 'Not Found',
+      statusCode: 404,
+    },
+  })
+  @ApiBadRequestResponse({
+    example: {
+      message: 'Id can only be a non-negative',
+      error: 'Bad Request',
+      statusCode: 400,
+    },
+  })
   async getId(
     @Param('id', ParseIntPipe) id: number
   ): Promise<{ hello_id: number }> {
