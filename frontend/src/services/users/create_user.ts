592a8aceac81bbe3e245464f3ccfc3fc5affa6fc
Jan Stránský <jan.stransky@profiq.com>
2026-02-20 07:56:54 +0000

    Resolve "FE: fix build errors"

--- frontend/src/services/users/create_user.ts
+++ frontend/src/services/users/create_user.ts
@@ -4,9 +4,8 @@ import {
   type APIResponse,
 } from '@/lib/api_client/api_client';
 import type { User } from '@/lib/contexts';
-import { StatusCodes } from 'http-status-codes';
 import type { UserType } from './users';
-import { createError, type MessageOverride } from '@/lib/errors';
+import { createError } from '@/lib/errors';
 
 export type CreateUserType = {
   name: string;
@@ -20,14 +19,8 @@ export async function createUser(data: CreateUserType, user?: User) {
     `/users`,
     data
   );
-  if (result.status_code != StatusCodes.CREATED) {
-    const overrides: MessageOverride[] = [
-      {
-        status_code: StatusCodes.NOT_FOUND,
-        message: 'The specified pet was not found',
-      },
-    ];
-    throw createError(result.status_code, overrides);
+  if (!result.success) {
+    throw createError(result.status_code, result.error.message);
   }
   return result.data;
 }
