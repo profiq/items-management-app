b9658ac3f71449c0ccfca6905e2d5f042fa699fb
Jan Stránský <jan.stransky@profiq.com>
2026-03-06 11:59:16 +0000

    feat(fe): add create a new visit button

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -4,3 +4,7 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export async function sleep(delay: number) {
+  return new Promise(res => setTimeout(res, delay));
+}
