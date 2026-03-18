66ed94a5deb870973a5b60ab089840f158d4e294
Jan Stránský <jan.stransky@profiq.com>
2026-03-18 16:26:51 +0000

    chore(fe): add profile page e2e

--- frontend/src/components/user-info.tsx
+++ frontend/src/components/user-info.tsx
@@ -7,20 +7,28 @@ function UserInfo() {
     return <></>;
   }
   return (
-    <>
-      <div>Email: {user.email}</div>
-      <div>UID: {user.uid}</div>
-      <div>Name: {user.displayName}</div>
-      <div>{user.phoneNumber ? `Phone number: ${user.phoneNumber}` : ''}</div>
+    <div data-testid='user-info'>
+      <div data-testid='user-email'>Email: {user.email}</div>
+      <div data-testid='user-uid'>UID: {user.uid}</div>
+      <div data-testid='user-name'>Name: {user.displayName}</div>
+      <div data-testid='user-phone'>
+        {user.phoneNumber ? `Phone number: ${user.phoneNumber}` : ''}
+      </div>
       {user.photoURL && (
-        <div className='flex flex-row items-center justify-center'>
+        <div
+          className='flex flex-row items-center justify-center'
+          data-testid='user-avatar-container'
+        >
           <Avatar>
-            <AvatarImage src={user.photoURL}></AvatarImage>
+            <AvatarImage
+              src={user.photoURL}
+              data-testid='user-avatar'
+            ></AvatarImage>
             <AvatarFallback>User avatar</AvatarFallback>
           </Avatar>
         </div>
       )}
-    </>
+    </div>
   );
 }
 export default UserInfo;
