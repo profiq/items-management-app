8c6a7526fea6dd9e6ee52f09c0953aff5036a71e
miroslav-matula <matulamiky@gmail.com>
2026-05-20 09:24:22 +0200

    Restore routes and components deleted by older branch; update to @profiq/ui imports

---
+++ frontend/src/lib/utils.ts
@@ -0,0 +1,10 @@
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export async function sleep(delay: number) {
+  return new Promise(res => setTimeout(res, delay));
+}
